a0f79d865750694711db2b381976fe3918b3b22a
Michael Abbott <michael@mjabbottdesign.com>
2021-08-08 12:18:01 -0700

    Add displays discount show page link, discount index feature test

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -13,13 +13,29 @@ RSpec.describe "The Merchant BulkDiscount index page" do
 
   it "displays the merchant's bulk discounts and their percentage and threshold attributes" do
     expect(page).to have_content(@discount1.id)
-    expect(page).to have_content(@discount1.percentage)
+    expect(page).to have_content("#{(@discount1.percentage * 100).round}%")
     expect(page).to have_content(@discount1.threshold)
     expect(page).to have_content(@discount2.id)
-    expect(page).to have_content(@discount2.percentage)
+    expect(page).to have_content("#{(@discount2.percentage * 100).round}%")
     expect(page).to have_content(@discount2.threshold)
     expect(page).to_not have_content(@discount3.id)
-    expect(page).to_not have_content(@discount3.percentage)
-    expect(page).to_not have_content(@discount3.threshold)
+  end
+
+  it "displays a link to the show page of each bulk discount listed" do
+    within "#all-discounts" do
+      within "#discount-#{@discount1.id}" do
+        expect(page).to_not have_link("#{@discount2.id}")
+        click_on("#{@discount1.id}")
+        expect(current_path).to eq(merchant_bulk_discount_path(@merchant1.id, @discount1.id))
+      end
+
+      visit merchant_bulk_discounts_path(@merchant1.id)
+
+      within "#discount-#{@discount2.id}" do
+        expect(page).to_not have_link("#{@discount1.id}")
+        click_on("#{@discount2.id}")
+        expect(current_path).to eq(merchant_bulk_discount_path(@merchant1.id, @discount2.id))
+      end
+    end
   end
 end
