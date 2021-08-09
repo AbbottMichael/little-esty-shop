2a474ce03b88e1a92bda8693fd2ba087cc9fa8f0
Michael Abbott <michael@mjabbottdesign.com>
2021-08-08 17:36:17 -0700

    Add Merchant Bulk Discount edit page feature test

---
+++ spec/features/merchants/bulk_discounts/edit_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "The Merchant Bulk Discount edit page" do
+  it "displays the discounts current attributes pre-poplulated in the form" do
+
+  end
+
+  it "only accepts valid input data"
+
+  it "displays an alert message when invalid data is entered"
+end
