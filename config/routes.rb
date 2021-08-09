7afb6ff90e5ed5d22b780216060a6a1cf11457b3
Michael Abbott <michael@mjabbottdesign.com>
2021-08-08 22:10:05 -0700

    Add :destroy action to bulk_discounts resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       resources :items
       resources :invoices
       resources :dashboard, only: [:index]
-      resources :bulk_discounts, only: [:index, :show, :edit, :update]
+      resources :bulk_discounts, only: [:index, :show, :edit, :update, :destroy]
   end
 
   namespace :admin do
