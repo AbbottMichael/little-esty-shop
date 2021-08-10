7435581a7f2f092ad9e146e01eb357514be8a31d
Michael Abbott <michael@mjabbottdesign.com>
2021-08-09 22:10:52 -0700

    Add invoice item #applied_discount

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,4 +7,13 @@ class InvoiceItem < ApplicationRecord
 
   belongs_to :invoice
   belongs_to :item
+
+  def applied_discount(merchant_id)
+    BulkDiscount.joins(merchant: [{ items: [{invoices: :invoice_items}]}])
+                .where(merchants: {id: merchant_id}, invoice_items: {invoice_id: self.invoice_id, id: self.id})
+                .where('invoice_items.quantity >= bulk_discounts.threshold')
+                .select('bulk_discounts.id', 'invoice_items.id AS invoice_item_id', 'invoice_items.quantity', 'bulk_discounts.threshold', 'items.id AS item_id')
+                .order(threshold: :desc)
+                .take
+  end
 end
