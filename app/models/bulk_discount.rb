db785d166d61c4847ab569bb293556fe2fc97666
Michael Abbott <michael@mjabbottdesign.com>
2021-08-09 21:48:27 -0700

    Remove whitespace

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,5 +3,4 @@ class BulkDiscount < ApplicationRecord
 
   validates :percentage, presence: true, numericality: { less_than: 1, greater_than: 0 }
   validates :threshold, presence: true, numericality: { only_integer: true, greater_than: 0 }
-
 end
